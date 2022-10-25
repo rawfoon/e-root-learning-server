10b538d27065225c32177e6dcac3f9f49d7e3354
rawfoon <mgrawfoon@gmail.com>
2022-10-25 18:32:05 +0600

    cors installed

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000
 
+app.use(cors())
 
+
+const categories = require('./data/categories.json')
 
 app.get('/', (req, res) =>{
     res.send('E-root api running')
 })
 
+app.get('/categoris', (req, res)=>{
+    res.send(categories)
+})
 
 app.listen(port, ()=>{
     console.log('e-root learning on port', port)
